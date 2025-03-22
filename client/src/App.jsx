0464aa4ce4b74366e15517c8873bba38d67f185b
Vaishnavi <VaishnaviTandra@users.noreply.github.com>
2025-03-22 20:41:00 +0530

    Title changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Blog Application</h1>
+      <h1>EchoVerse</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
